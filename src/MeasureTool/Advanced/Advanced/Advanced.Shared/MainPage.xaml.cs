1c3b914f06a36f4aedb18f02d55c6fa528d89425
jenn6286 <jnery@esri.com>
2015-03-13 10:38:31 -0700

    Updated project reference to use nuget package ArcGIS Runtime SDK for .NET 10.2.5. Fixed sample description and some code cleanup.

--- src/MeasureTool/Advanced/Advanced/Advanced.Shared/MainPage.xaml.cs
+++ src/MeasureTool/Advanced/Advanced/Advanced.Shared/MainPage.xaml.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Windows.UI;
 using Windows.UI.Popups;
@@ -15,128 +14,136 @@ using Windows.UI.Xaml.Controls;
 
 namespace Advanced
 {
-    /// <summary>
-    /// This sample demonstrates how to the Editor is used sketching geometries
-    /// and how it can be controlled to allow you to interact with the map without 
-    /// affecting the geometry being digitized. In addition, it shows how you can
-    /// customize the symbology used by the edit tools and track the sketch progress.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        private ObservableCollection<string> measurements;
+	/// <summary>
+	/// This sample demonstrates how the Editor is used for sketching geometries
+	/// and how to interact with the map without affecting the geometry being digitized. 
+	/// This sample also shows how to override the default symbology as well as track the sketch progress.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		private readonly ObservableCollection<string> _measurements;
 
-        public MainPage()
-        {
-            this.InitializeComponent();
-            measurements = new ObservableCollection<string>();
-            Measurements.ItemsSource = measurements;
-        }
+		public MainPage()
+		{
+			this.InitializeComponent();
+			_measurements = new ObservableCollection<string>();
+			Measurements.ItemsSource = _measurements;
+		}
 
-        private async void MeasureButton_Click(object sender, RoutedEventArgs e)
-        {
-            string message = null;
-            try
-            {
-                var symbol = this.Resources["PolylineSymbol"] as LineSymbol;
-                var progress = new Progress<GeometryEditStatus>(OnStatusUpdated);
-                var geometry = await MyMapView.Editor.RequestShapeAsync(DrawShape.Polyline, symbol, progress);            
-                var layer = MyMapView.Map.Layers["ResultLayer"] as GraphicsLayer;
-                layer.Graphics.Clear();
-                measurements.Clear();
-                TotalLength.Text = TotalArea.Text = string.Empty;
-            }
-            catch (TaskCanceledException tc)
-            {
-            }
-            catch (Exception ex)
-            {
-                message = ex.Message;
-            }
-            if (!string.IsNullOrWhiteSpace(message))
-                await new MessageDialog(message).ShowAsync();
-        }
+		private async void MeasureButton_Click(object sender, RoutedEventArgs e)
+		{
+			string message = null;
+			try
+			{
+				var symbol = this.Resources["PolylineSymbol"] as LineSymbol;
+				var progress = new Progress<GeometryEditStatus>(OnStatusUpdated);
+				var geometry = await MyMapView.Editor.RequestShapeAsync(DrawShape.Polyline, symbol, progress);
+				var layer = MyMapView.Map.Layers["ResultLayer"] as GraphicsLayer;
+				if (layer == null)
+					return;
+				layer.Graphics.Clear();
+				_measurements.Clear();
+				TotalLength.Text = TotalArea.Text = string.Empty;
+			}
+			catch (TaskCanceledException)
+			{
+			}
+			catch (Exception ex)
+			{
+				message = ex.Message;
+			}
+			if (!string.IsNullOrWhiteSpace(message))
+				await new MessageDialog(message).ShowAsync();
+		}
 
-        private void OnStatusUpdated(GeometryEditStatus status)
-        {
-            measurements.Clear();
-            var polyline = status.NewGeometry as Polyline;
-            if (polyline == null || polyline.Parts == null || polyline.Parts.Count == 0)
-                return;
-            var vertices = polyline.Parts[0].GetPoints();
-            var count = vertices != null ? vertices.Count() : 0;
-            if (count <= 1)
-                return;
-            var result = new StringBuilder();
-            MapPoint previous = null;
-            int i = 1;
-            foreach (var point in vertices)
-            {
-                if (previous == null)
-                {
-                    previous = point;
-                    continue;
-                }
-                var lineSegment = new Polyline(new MapPoint[] { previous, point }, polyline.SpatialReference);
-                var intermediateLength = GeometryEngine.GeodesicLength(lineSegment);
-                measurements.Add(string.Format("[{0}-{1}]\t:\t{2:0} m\n", i, i + 1, intermediateLength));
-                i++;
-            }
-            var totalLength = GeometryEngine.GeodesicLength(polyline);
-            TotalLength.Text = string.Format("Total Length\t:\t{0:0} m\n", totalLength);
-            if (count <= 2)
-                return;
-            var layer = MyMapView.Map.Layers["ResultLayer"] as GraphicsLayer;
-            var graphic = layer.Graphics.FirstOrDefault();
-            var polygon = new Polygon(vertices, polyline.SpatialReference);
-            if (graphic != null)
-                graphic.Geometry = polygon;
-            else
-                layer.Graphics.Add(new Graphic() { Geometry = polygon });
-            if (count <= 1)
-                return;
-            if (count <= 2)
-                return;
-            var area = GeometryEngine.GeodesicArea(polygon);           
-            TotalArea.Text = string.Format("Area\t\t:\t{0:0} m²\n", area);
-        }
+		private void OnStatusUpdated(GeometryEditStatus status)
+		{
+			_measurements.Clear();
+			var polyline = status.NewGeometry as Polyline;
+			if (polyline == null || polyline.Parts == null || polyline.Parts.Count == 0)
+				return;
+			var vertices = polyline.Parts[0].GetPoints();
+			if (vertices == null || !vertices.Any())
+				return;
+			var count = vertices.Count();
+			if (count <= 1)
+				return;
+			MapPoint previous = null;
+			int i = 1;
+			foreach (var point in vertices)
+			{
+				if (previous == null)
+				{
+					previous = point;
+					continue;
+				}
+				var lineSegment = new Polyline(new MapPoint[] {previous, point}, polyline.SpatialReference);
+				var intermediateLength = GeometryEngine.GeodesicLength(lineSegment);
+				_measurements.Add(string.Format("[{0}-{1}]\t:\t{2:0} m\n", i, i + 1, intermediateLength));
+				i++;
+			}
+			var totalLength = GeometryEngine.GeodesicLength(polyline);
+			TotalLength.Text = string.Format("Total Length\t:\t{0:0} m\n", totalLength);
+			if (count <= 2)
+				return;
+			var layer = MyMapView.Map.Layers["ResultLayer"] as GraphicsLayer; 
+			if (layer == null)
+				return;
+			var graphic = layer.Graphics.FirstOrDefault();
+			var polygon = new Polygon(vertices, polyline.SpatialReference);
+			if (graphic != null)
+				graphic.Geometry = polygon;
+			else
+				layer.Graphics.Add(new Graphic(polygon));
+			if (count <= 1)
+				return;
+			if (count <= 2)
+				return;
+			var area = GeometryEngine.GeodesicArea(polygon);
+			TotalArea.Text = string.Format("Area\t\t:\t{0:0} m²\n", area);
+		}
 
-        private void SuspendButton_Click(object sender, RoutedEventArgs e)
-        {
-            var isSuspended = MyMapView.Editor.IsSuspended;
-            (sender as AppBarButton).Icon = isSuspended ? new SymbolIcon(Windows.UI.Xaml.Controls.Symbol.Pause) : new SymbolIcon(Windows.UI.Xaml.Controls.Symbol.Play);
-                (sender as AppBarButton).Label = isSuspended ? "Suspend" : "Resume";
-            MyMapView.Editor.IsSuspended = !isSuspended;            
-        }
-    }
+		private void SuspendButton_Click(object sender, RoutedEventArgs e)
+		{
+			var button = (AppBarButton) sender;
+			var isSuspended = MyMapView.Editor.IsSuspended;
+			button.Icon = isSuspended
+				? new SymbolIcon(Windows.UI.Xaml.Controls.Symbol.Pause)
+				: new SymbolIcon(Windows.UI.Xaml.Controls.Symbol.Play);
+			button.Label = isSuspended ? "Suspend" : "Resume";
+			MyMapView.Editor.IsSuspended = !isSuspended;
+		}
+	}
 
-    public class MeasureEditor : Editor
-    {
-        protected override Esri.ArcGISRuntime.Symbology.Symbol OnGenerateSymbol(GenerateSymbolInfo generateSymbolInfo)
-        {
-            if(generateSymbolInfo.GenerateSymbolType == GenerateSymbolType.Vertex || generateSymbolInfo.GenerateSymbolType == GenerateSymbolType.SelectedVertex)
-            {
-                var index = generateSymbolInfo.VertexPosition.CoordinateIndex + 1;
-                return new CompositeSymbol()
-                {
-                    Symbols = new SymbolCollection(new Esri.ArcGISRuntime.Symbology.Symbol[]
-                        {
-                            new SimpleMarkerSymbol()
-                            {
-                                Color = Colors.White,
-                                Size = 14.5,
-                                Outline = new SimpleLineSymbol() {Width = 1.5, Color = Colors.CornflowerBlue},
-                            },
-                            new TextSymbol()
-                            {
-                                Text =
-                                    Convert.ToString(index, CultureInfo.InvariantCulture),
-                                HorizontalTextAlignment = HorizontalTextAlignment.Center,
-                                VerticalTextAlignment = VerticalTextAlignment.Middle
-                            },
-                        })
-                };
-            }
-            return base.OnGenerateSymbol(generateSymbolInfo);
-        }
-    }
+	public class MeasureEditor : Editor
+	{
+		protected override Esri.ArcGISRuntime.Symbology.Symbol OnGenerateSymbol(GenerateSymbolInfo generateSymbolInfo)
+		{
+			if (generateSymbolInfo.GenerateSymbolType == GenerateSymbolType.Vertex ||
+			    generateSymbolInfo.GenerateSymbolType == GenerateSymbolType.SelectedVertex)
+			{
+				var index = generateSymbolInfo.VertexPosition.CoordinateIndex + 1;
+				return new CompositeSymbol()
+				{
+					Symbols = new SymbolCollection(new Esri.ArcGISRuntime.Symbology.Symbol[]
+					{
+						new SimpleMarkerSymbol
+						{
+							Color = Colors.White,
+							Size = 14.5,
+							Outline = new SimpleLineSymbol() {Width = 1.5, Color = Colors.CornflowerBlue},
+						},
+						new TextSymbol
+						{
+							Text =
+								Convert.ToString(index, CultureInfo.InvariantCulture),
+							HorizontalTextAlignment = HorizontalTextAlignment.Center,
+							VerticalTextAlignment = VerticalTextAlignment.Middle
+						}
+					})
+				};
+			}
+			return base.OnGenerateSymbol(generateSymbolInfo);
+		}
+	}
 }
