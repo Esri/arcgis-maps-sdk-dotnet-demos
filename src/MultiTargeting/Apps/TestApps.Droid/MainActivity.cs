e26a7fa96a8b370ee0c3036a97f2b4c50fc1469b
dotMorten <mnielsen@esri.com>
2018-02-20 11:16:52 -0800

    Added Multi targeting project sample

---
+++ src/MultiTargeting/Apps/TestApps.Droid/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Security;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.UI.Controls;
+
+namespace TestApps.Droid
+{
+    [Activity (Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Activity
+    {
+        private MultiTargetLibrary.MapViewModel _mapViewModel = new MultiTargetLibrary.MapViewModel();
+        private MapView _mapView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set the view from the "Main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get MapView from the view and assign map from view-model
+            _mapView = FindViewById<MapView>(Resource.Id.MyMapView);
+            _mapView.Map = _mapViewModel.Map;
+
+            // Listen for changes on the view model
+            _mapViewModel.PropertyChanged += MapViewModel_PropertyChanged;
+        }
+
+        private void MapViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // Update the map view with the view model's new map
+            if (e.PropertyName == nameof(MultiTargetLibrary.MapViewModel.Map) && _mapView != null)
+                _mapView.Map = _mapViewModel.Map;
+        }
+    }
+}
---
+++ src/MultiTargeting/README.md
@@ -0,0 +1,10 @@
+Demo: Multi Targeting Project
+============================
+Required version: ArcGIS Runtime SDK 100.2.1 for .NET 
+
+
+Demonstrates how to create a single multi-targeting project that targets all the frameworks that the ArcGIS Runtime SDK for .NET Supports, which can then be referenced by WPF, Windows Universal, iOS, Android and Xamarin.Forms projects.
+
+
+![Screenshot](Screenshot.png)
+
