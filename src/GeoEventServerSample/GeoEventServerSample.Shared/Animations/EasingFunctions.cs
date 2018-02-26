c1f985948594b3a365df31219a94439fe753bf63
dorMorten <no@spam.thx>
2018-02-26 15:21:18 -0800

    Clean up

--- src/GeoEventServerSample/GeoEventServerSample.Shared/Animations/EasingFunctions.cs
+++ src/GeoEventServerSample/GeoEventServerSample.Shared/Animations/EasingFunctions.cs
@@ -1,6 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// /*******************************************************************************
+//  * Copyright 2012-2018 Esri
+//  *
+//  *  Licensed under the Apache License, Version 2.0 (the "License");
+//  *  you may not use this file except in compliance with the License.
+//  *  You may obtain a copy of the License at
+//  *
+//  *  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  *   Unless required by applicable law or agreed to in writing, software
+//  *   distributed under the License is distributed on an "AS IS" BASIS,
+//  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  *   See the License for the specific language governing permissions and
+//  *   limitations under the License.
+//  ******************************************************************************/
 
 namespace GeoEventServerSample.Animations
 {
@@ -14,20 +26,26 @@ namespace GeoEventServerSample.Animations
     public abstract class EasingFunctionBase
     {
         protected EasingFunctionBase() { }
+
         public EasingMode EasingMode { get; set; }
+
         public double Ease(double normalizedTime)
         {
-            switch (EasingMode)
+            if (EasingMode == EasingMode.EaseOut)
             {
-                case EasingMode.EaseOut:
-                    return 1.0 - EasingImplementation(1.0 - normalizedTime);
-                case EasingMode.EaseIn:
-                    return EasingImplementation(normalizedTime);
-                case EasingMode.EaseInOut:
-                default:
-                    return (normalizedTime < 0.5) ? EasingImplementation(normalizedTime * 2.0) * 0.5 : (1.0 - EasingImplementation((1.0 - normalizedTime) * 2.0)) * 0.5 + 0.5;
+                return 1 - EasingImplementation(1 - normalizedTime);
             }
-
+            else if (EasingMode == EasingMode.EaseInOut)
+            {
+                if (normalizedTime < .5)
+                    return EasingImplementation(normalizedTime * 2.0) * 0.5;
+                else
+                    return (1.0 - EasingImplementation((1.0 - normalizedTime) * 2.0)) * 0.5 + 0.5;
+            }
+            else // if (EasingMode == EasingMode.EaseIn)
+            {
+                return EasingImplementation(normalizedTime);
+            }
         }
 
         protected abstract double EasingImplementation(double normalizedTime);
