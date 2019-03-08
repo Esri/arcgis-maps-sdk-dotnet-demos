8d099cc0408a53faaf7a61be50d117c51d084f23
Nathan Castle <ncastle@esri.com>
2019-03-08 14:13:33 -0800

    undo change to multitarget

--- src/MultiTargeting/Apps/TestApps.Droid/Resources/Resource.Designer.cs
+++ src/MultiTargeting/Apps/TestApps.Droid/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace TestApps.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int esri = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
