328eb233fb258ab49a129788bd002fbbba20cc18
erandis-vol <jack.bulan@gmail.com>
2020-05-21 11:21:32 -0700

    Add login status to Xamarin

--- src/TurnByTurn/RoutingSample.Forms/RoutingSample.Forms/LoginPage.xaml.cs
+++ src/TurnByTurn/RoutingSample.Forms/RoutingSample.Forms/LoginPage.xaml.cs
@@ -20,17 +20,19 @@ namespace RoutingSample.Forms
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
+            LoginStatus.Text = "Signing in...";
             var username = Username.Text.Trim();
             var password = Password.Text.Trim();
             try
             {
                 var credential = await AuthenticationManager.Current.GenerateCredentialAsync(new Uri("https://www.arcgis.com/sharing/rest"), username, password);
                 AuthenticationManager.Current.AddCredential(credential);
+                LoginStatus.Text = "Success!";
                 await Navigation.PopModalAsync();
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Error: " + ex);
+                LoginStatus.Text = "Error: " + ex.Message;
             }
         }
     }
