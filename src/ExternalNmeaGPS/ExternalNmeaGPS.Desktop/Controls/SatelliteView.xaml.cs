99ad16302cba10a4b7c27e7ae117ca1aed8ad544
Morten Nielsen <mnielsen@esri.com>
2023-11-27 10:34:55 -0800

    Use the new SNR property to show satellite strengths

--- src/ExternalNmeaGPS/ExternalNmeaGPS.Desktop/Controls/SatelliteView.xaml.cs
+++ src/ExternalNmeaGPS/ExternalNmeaGPS.Desktop/Controls/SatelliteView.xaml.cs
@@ -15,80 +15,79 @@ using System.Windows.Shapes;
 
 namespace ExternalNmeaGPS.Controls
 {
-	/// <summary>
-	/// Interaction logic for SatelliteView.xaml
-	/// </summary>
-	public partial class SatelliteView : UserControl
-	{
-		public SatelliteView()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for SatelliteView.xaml
+    /// </summary>
+    public partial class SatelliteView : UserControl
+    {
+        public SatelliteView()
+        {
+            InitializeComponent();
+        }
 
-		public IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo> SatelliteInfos
-		{
-			get { return (IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo>)GetValue(GsvMessageProperty); }
-			set { SetValue(GsvMessageProperty, value); }
-		}
+        public IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo> SatelliteInfos
+        {
+            get { return (IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo>)GetValue(SatelliteInfosProperty); }
+            set { SetValue(SatelliteInfosProperty, value); }
+        }
 
-		public static readonly DependencyProperty GsvMessageProperty =
-			DependencyProperty.Register(nameof(SatelliteInfos), typeof(IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo>), typeof(SatelliteView), new PropertyMetadata(null, OnGsvMessagePropertyChanged));
+        public static readonly DependencyProperty SatelliteInfosProperty =
+            DependencyProperty.Register(nameof(SatelliteInfos), typeof(IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo>), typeof(SatelliteView), new PropertyMetadata(null, OnSatelliteInfosPropertyChanged));
 
-		private static void OnGsvMessagePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-		{
-			var sats = e.NewValue as IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo>;
-			if (sats == null)
-				((SatelliteView)d).satellites.ItemsSource = null;
-			else
-				((SatelliteView)d).satellites.ItemsSource = sats;
-		}		
-	}
-	public class PolarPlacementItem : ContentControl
-	{
-		public PolarPlacementItem()
-		{
-			HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
-			VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
-		}
-		protected override Size ArrangeOverride(Size arrangeBounds)
-		{
-			double az = (Azimuth - 90) / 180 * Math.PI;
-			double e = (90 - Elevation) / 90;
-			double X = Math.Cos(az) * e;
-			double Y = Math.Sin(az) * e;
-			X = arrangeBounds.Width * .5 * X;
-			Y = arrangeBounds.Height * .5 * Y;
-			RenderTransform = new TranslateTransform(X, Y);
-			return base.ArrangeOverride(arrangeBounds);
-		}
+        private static void OnSatelliteInfosPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var sats = e.NewValue as IReadOnlyList<Esri.ArcGISRuntime.Location.NmeaSatelliteInfo>;
+            if (sats == null)
+                ((SatelliteView)d).satellites.ItemsSource = null;
+            else
+                ((SatelliteView)d).satellites.ItemsSource = sats;
+        }
+    }
+    public class PolarPlacementItem : ContentControl
+    {
+        public PolarPlacementItem()
+        {
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+        }
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            double az = (Azimuth - 90) / 180 * Math.PI;
+            double e = (90 - Elevation) / 90;
+            double X = Math.Cos(az) * e;
+            double Y = Math.Sin(az) * e;
+            X = arrangeBounds.Width * .5 * X;
+            Y = arrangeBounds.Height * .5 * Y;
+            RenderTransform = new TranslateTransform(X, Y);
+            return base.ArrangeOverride(arrangeBounds);
+        }
 
-		public double Azimuth
-		{
-			get { return (double)GetValue(AzimuthProperty); }
-			set { SetValue(AzimuthProperty, value); }
-		}
+        public double Azimuth
+        {
+            get { return (double)GetValue(AzimuthProperty); }
+            set { SetValue(AzimuthProperty, value); }
+        }
 
-		public static readonly DependencyProperty AzimuthProperty =
-			DependencyProperty.Register("Azimuth", typeof(double), typeof(PolarPlacementItem), new PropertyMetadata(0d, OnAzimuthPropertyChanged));
+        public static readonly DependencyProperty AzimuthProperty =
+            DependencyProperty.Register("Azimuth", typeof(double), typeof(PolarPlacementItem), new PropertyMetadata(0d, OnAzimuthPropertyChanged));
 
-		private static void OnAzimuthPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-		{
-			((UIElement)d).InvalidateArrange();
-		}
+        private static void OnAzimuthPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((UIElement)d).InvalidateArrange();
+        }
 
-		public double Elevation
-		{
-			get { return (double)GetValue(ElevationProperty); }
-			set { SetValue(ElevationProperty, value); }
-		}
+        public double Elevation
+        {
+            get { return (double)GetValue(ElevationProperty); }
+            set { SetValue(ElevationProperty, value); }
+        }
 
-		public static readonly DependencyProperty ElevationProperty =
-			DependencyProperty.Register("Elevation", typeof(double), typeof(PolarPlacementItem), new PropertyMetadata(0d, OnElevationPropertyChanged));
+        public static readonly DependencyProperty ElevationProperty =
+            DependencyProperty.Register("Elevation", typeof(double), typeof(PolarPlacementItem), new PropertyMetadata(0d, OnElevationPropertyChanged));
 
-		private static void OnElevationPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-		{
-			((UIElement)d).InvalidateArrange();
-		}
-
-	}
+        private static void OnElevationPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((UIElement)d).InvalidateArrange();
+        }
+    }
 }
