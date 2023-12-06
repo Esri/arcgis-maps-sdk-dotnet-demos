2628f7941d4ff298eec9ef496174ef708a8e8aa2
Morten Nielsen <mort5161@esri.com>
2023-12-05 17:11:11 -0800

    Initial work on geometry editor toolbar sample

---
+++ src/EditorDemo/EditorToolbarController.Commands.cs
@@ -0,0 +1,121 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorDemo
+{
+    internal partial class EditorToolbarController
+    {
+        private bool CanEditVertices => CanEditGeometry(GeoElement) && (GeoElement?.Geometry is Polygon || (GeoElement?.Geometry is Polyline));
+
+        [RelayCommand(CanExecute = nameof(CanEditVertices))]
+        private void EditVertices()
+        {
+            GeometryEditor = editor;
+            editor.EditVertices();
+        }
+        private bool CanMove => CanEditGeometry(GeoElement) == true;
+
+        [RelayCommand(CanExecute = nameof(CanMove))]
+        private void Move()
+        {
+            GeometryEditor = editor;
+            editor.Move();
+        }
+
+        private bool CanUndo() => editor.CanUndo;
+        [RelayCommand(CanExecute = nameof(CanUndo))]
+        private void Undo() => editor.Undo();
+
+        private bool CanRedo() => editor.CanRedo;
+        [RelayCommand(CanExecute = nameof(CanRedo))]
+        private void Redo() => editor.Redo();
+
+        [RelayCommand(CanExecute = nameof(CanEditVertices))]
+        private void Rotate()
+        {
+            GeometryEditor = editor;
+            editor.Rotate();
+        }
+
+        private static void SetElementVisibility(bool visible, GeoElement? element)
+        {
+            if (element is Graphic g)
+                g.IsVisible = visible;
+            else if (element is Feature f && f.FeatureTable?.Layer is FeatureLayer l)
+            {
+                l.SetFeatureVisible(f, visible);
+            }
+        }
+
+        private bool CanReshape => !reshapeEditor.IsStarted && editor.Geometry?.IsEmpty == false && editor.Geometry is Polygon; //TODO
+
+        [RelayCommand(CanExecute = nameof(CanReshape))]
+        private void Reshape()
+        {
+            GeometryEditor = reshapeEditor;
+            reshapeEditor.Start(GeometryType.Polyline);
+            ReshapeCommand.NotifyCanExecuteChanged();
+            ReshapeAcceptCommand.NotifyCanExecuteChanged();
+            ReshapeDiscardCommand.NotifyCanExecuteChanged();
+        }
+
+        private bool CanAcceptReshape => reshapeEditor.IsStarted &&
+            this.editor.Geometry is Multipart mp && !mp.IsEmpty && reshapeEditor.Geometry is Polyline line && !line.IsEmpty &&
+            GeometryEngine.Reshape(mp, line) != null;
+
+        [RelayCommand(CanExecute = nameof(CanAcceptReshape))]
+        private void ReshapeAccept()
+        {
+            var geometry = reshapeEditor.Stop();
+            EditorOverlay.Graphics.Clear();
+            if (editor.Geometry is Multipart mp && geometry is Polyline line && !mp.IsEmpty && !line.IsEmpty)
+            {
+                var reshapedGeometry = GeometryEngine.Reshape(mp, line);
+                if (reshapedGeometry != null)
+                    editor.ReplaceGeometry(reshapedGeometry);
+            }
+            GeometryEditor = editor;
+            ReshapeCommand.NotifyCanExecuteChanged();
+            ReshapeAcceptCommand.NotifyCanExecuteChanged();
+            ReshapeDiscardCommand.NotifyCanExecuteChanged();
+        }
+
+        private bool CanReshapeDiscard => reshapeEditor.IsStarted;
+
+        [RelayCommand(CanExecute = nameof(CanReshapeDiscard))]
+        private void ReshapeDiscard()
+        {
+            reshapeEditor.Stop();
+            EditorOverlay.Graphics.Clear();
+            GeometryEditor = editor;
+            ReshapeCommand.NotifyCanExecuteChanged();
+            ReshapeAcceptCommand.NotifyCanExecuteChanged();
+            ReshapeDiscardCommand.NotifyCanExecuteChanged();
+        }
+
+        private bool CanApply => CanEditGeometry(GeoElement) && editor.IsStarted && GeoElement != null &&(editor.Geometry?.IsEmpty ?? true) == false;
+
+        [RelayCommand(CanExecute = nameof(CanApply))]
+        private void Apply()
+        {
+            Debug.Assert(GeoElement != null);
+            var geometry = editor.Stop();
+            if (geometry != null) 
+                EditingCompleted?.Invoke(this, geometry);
+        }
+
+        private bool CanDiscard => GeoElement != null;
+
+        [RelayCommand(CanExecute = nameof(CanDiscard))]
+        private void Discard() => EditingCancelled?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ src/EditorDemo/MapViewModel.cs
@@ -0,0 +1,139 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Toolkit.UI;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.UI.Controls;
+using Esri.ArcGISRuntime.UI.Editing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EditorDemo
+{
+    public partial class MapViewModel : ObservableObject
+    {
+        public MapViewModel()
+        {
+            LoadMap();
+        }
+
+        private async void LoadMap()
+        {
+            Map = await MapCreator.CreateMap(true);
+        }
+
+        public MapViewController Controller { get; } = new MapViewController();
+
+        [ObservableProperty]
+        private Map? _map;
+
+        partial void OnMapChanging(Map? value)
+        {
+            // Clean up
+            Controller.DismissCallout();
+            Selection = null;
+            EditFeatureSelection = null;
+        }
+
+        public GraphicsOverlayCollection? GraphicsOverlays { get; } = new GraphicsOverlayCollection();
+
+        [RelayCommand]
+        public async Task OnGeoViewTapped(GeoViewInputEventArgs eventArgs) => await Identify(eventArgs.Position, eventArgs.Location);
+
+        public async Task Identify(Point location, MapPoint? mapLocation)
+        {
+            if (EditFeatureSelection is not null)
+                return; // Don't do identify during an edit session
+            Controller.DismissCallout();
+            Selection = null;
+            var result = await Controller.IdentifyLayersAsync(location, 1);
+            if (result.FirstOrDefault()?.GeoElements?.FirstOrDefault() is GeoElement element)
+            {
+                var def = new CalloutDefinition(element) { TextExpression = "$feature.name" };
+                Selection = element;
+                def.Tag = element;
+                if (EditorToolbar.CanEditGeometry(element))
+                {
+                    def.ButtonImage = new RuntimeImage(new Uri("edit-geometry-16.png", UriKind.Relative));
+                    def.OnButtonClick = (obj) => Edit(obj as GeoElement);
+                }
+                Controller.ShowCalloutForGeoElement(element, location, def);
+            }
+            else if (mapLocation is not null)
+            {
+                Controller.ShowCalloutAt(mapLocation, new CalloutDefinition("No features found"));
+            }
+        }
+
+        private void Edit(GeoElement? element)
+        {
+            if (element?.Geometry is not null)
+            {
+                EditFeatureSelection = element;
+                Controller.DismissCallout();
+                Controller.SetViewpointGeometryAsync(element.Geometry, 50);
+            }
+        }
+
+        [RelayCommand]
+        public void OnEditingCompleted(EditorToolbar.EditingCompletedEventArgs e)
+        {
+            if (EditFeatureSelection != null)
+            {
+                EditFeatureSelection.Geometry = e.Geometry;
+                if(EditFeatureSelection is Feature feature && feature.FeatureTable?.CanUpdate(feature) == true)
+                {
+                    feature.FeatureTable.UpdateFeatureAsync(feature);
+                }
+                EditFeatureSelection = null;
+            }
+        }
+
+        [RelayCommand]
+        public void OnEditingCancelled()
+        {
+            EditFeatureSelection = null;
+        }
+
+        [ObservableProperty]
+        private GeoElement? _editFeatureSelection;
+
+        [ObservableProperty]
+        private GeoElement? _selection;
+
+        partial void OnSelectionChanged(GeoElement? oldValue, GeoElement? newValue)
+        {
+            Select(oldValue, false);
+            Select(newValue, true);
+        }
+
+        private void Select(GeoElement? value, bool selected)
+        {
+            if (value is Graphic g)
+                g.IsSelected = selected;
+            else if (value is Feature feature && feature.FeatureTable?.Layer is FeatureLayer layer)
+            {
+                if (selected) layer.SelectFeature(feature);
+                else layer.UnselectFeature(feature);
+            }
+        }
+    }
+
+    public class MapViewController  : GeoViewController
+    {
+        public Task<bool> SetViewpointGeometryAsync(Geometry? boundingGeometry, double padding = 0d)
+        {
+            if(base.ConnectedView is MapView mapView && boundingGeometry is not null)
+            {
+                return mapView.SetViewpointGeometryAsync(boundingGeometry, padding);
+            }
+            return Task.FromResult(false);
+        }
+    }
+}
