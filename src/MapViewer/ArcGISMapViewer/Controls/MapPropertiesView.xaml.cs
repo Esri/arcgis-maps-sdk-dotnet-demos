4f6b26dcf961f9e3b5a9034c6c1b22faf0782698
dotMorten <mn@iter.dk>
2024-08-20 20:51:20 -0700

    Add featurelayer properties

--- src/MapViewer/ArcGISMapViewer/Controls/MapPropertiesView.xaml.cs
+++ src/MapViewer/ArcGISMapViewer/Controls/MapPropertiesView.xaml.cs
@@ -40,5 +40,39 @@ namespace ArcGISMapViewer.Controls
             DependencyProperty.Register("GeoModel", typeof(GeoModel), typeof(MapPropertiesView), new PropertyMetadata(null));
 
 
+
+        public object SelectedLayer
+        {
+            get { return (object)GetValue(SelectedLayerProperty); }
+            set { SetValue(SelectedLayerProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedLayerProperty =
+            DependencyProperty.Register("SelectedLayer", typeof(object), typeof(MapPropertiesView), new PropertyMetadata(null));
+
+
+    }
+    public class LayerTemplateSelector : DataTemplateSelector
+    {
+        public LayerTemplateSelector()
+        {
+
+        }
+        protected override DataTemplate? SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is Esri.ArcGISRuntime.Data.FeatureTable)
+                return FeatureTableTemplate;
+            if (item is FeatureLayer)
+                return FeatureLayerTemplate;
+            else if (item is Layer)
+                return LayerTemplate;
+            else if (item is ILayerContent)
+                return LayerContentTemplate;
+            return base.SelectTemplateCore(item, container);
+        }
+        public DataTemplate? FeatureLayerTemplate { get; set; }
+        public DataTemplate? FeatureTableTemplate { get; set; }
+        public DataTemplate? LayerTemplate { get; set; }
+        public DataTemplate? LayerContentTemplate { get; set; }
     }
 }
