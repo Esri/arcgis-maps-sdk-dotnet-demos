5c30c271290e52aa28fdaf66a5ac01bde1575810
dotMorten <mn@iter.dk>
2025-03-25 10:22:54 -0700

    Use x:bind in control templates

---
+++ src/MapViewer/ArcGISMapViewer.Controls/Table/FeatureTableView.Themes.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace ArcGISMapViewer.Controls;
+
+internal sealed partial class FeatureTableViewResources : ResourceDictionary
+{
+    public FeatureTableViewResources()
+    {
+        InitializeComponent();
+    }
+}
