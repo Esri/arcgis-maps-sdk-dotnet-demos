2a59e6b67eb20db1632565f48309da34a41de5b5
dotMorten <mn@iter.dk>
2025-12-03 20:23:37 -0800

    Finish WindowPanel

---
+++ src/MapViewer/ArcGISMapViewer.Controls/CursorContentControl.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ArcGISMapViewer.Controls
+{
+    public partial class CursorContentControl : ContentControl
+    {
+        public CursorContentControl()
+        {
+            HorizontalContentAlignment = HorizontalAlignment.Stretch;
+        }
+        public Windows.UI.Core.CoreCursorType Cursor
+        {
+            get { return (Windows.UI.Core.CoreCursorType)GetValue(CursorProperty); }
+            set { SetValue(CursorProperty, value); }
+        }
+
+        public static readonly DependencyProperty CursorProperty =
+            DependencyProperty.Register(nameof(Cursor), typeof(Windows.UI.Core.CoreCursorType), typeof(CursorContentControl), new PropertyMetadata(Windows.UI.Core.CoreCursorType.Arrow, (s,e)=>((CursorContentControl)s).OnCursorPropertyChanged()));
+
+        private void OnCursorPropertyChanged()
+        {
+            ProtectedCursor = Microsoft.UI.Input.InputCursor.CreateFromCoreCursor(new Windows.UI.Core.CoreCursor(Cursor, 0));
+        }
+    }
+}
--- src/MapViewer/ArcGISMapViewer.Controls/WindowPanel.cs
+++ src/MapViewer/ArcGISMapViewer.Controls/WindowPanel.cs
@@ -20,6 +20,40 @@ namespace ArcGISMapViewer.Controls
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+            if (GetTemplateChild("CollapseButton") is Button collapseButton)
+            {
+                collapseButton.Click += (s, e) =>
+                {
+                    IsCollapsed = !IsCollapsed;
+                };
+            }
+            if (GetTemplateChild("CloseButton") is Button closeButton)
+            {
+                closeButton.Click += (s, e) =>
+                {
+                    CloseRequested?.Invoke(this, EventArgs.Empty);
+                };
+            }
+            if (GetTemplateChild("ResizeThumb") is UIElement elm)
+            {
+                elm.ManipulationDelta += ResizeThumb_ManipulationDelta;
+            }
+            OnIsCollapsedPropertyChanged(false);
+            OnStatePropertyChanged(false);
+        }
+
+        private void ResizeThumb_ManipulationDelta(object sender, Microsoft.UI.Xaml.Input.ManipulationDeltaRoutedEventArgs e)
+        {
+            if(e.Delta.Translation.Y != 0)
+            {
+                var parent = GetTemplateChild("LayoutRoot") as FrameworkElement;
+                if (parent is not null)
+                {
+                    var newHeight = parent.ActualHeight - e.Delta.Translation.Y;
+                    var totalHeight = parent.ActualHeight / HeightRatio;
+                    HeightRatio = Math.Min(1, Math.Max(0, newHeight / totalHeight));
+                }
+            }
         }
 
         public object? Header
@@ -51,5 +85,46 @@ namespace ArcGISMapViewer.Controls
 
         public event EventHandler? CloseRequested;
 
+        public bool IsCollapsed
+        {
+            get { return (bool)GetValue(IsCollapsedProperty); }
+            set { SetValue(IsCollapsedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsCollapsedProperty =
+            DependencyProperty.Register(nameof(IsCollapsed), typeof(bool), typeof(WindowPanel), new PropertyMetadata(false, (s, e) => ((WindowPanel)s).OnIsCollapsedPropertyChanged()));
+
+        private void OnIsCollapsedPropertyChanged(bool useTransitions = true)
+        {
+            VisualStateManager.GoToState(this, IsCollapsed ? "Collapsed" : "Expanded", useTransitions);
+            var bottomRow = GetTemplateChild("BottomRow") as RowDefinition;
+            if(bottomRow is not null)
+            {
+                if (IsCollapsed)
+                    bottomRow.Height = new GridLength(1, GridUnitType.Auto);
+                else
+                    OnStatePropertyChanged();
+            }
+        }
+
+        public double HeightRatio
+        {
+            get { return (double)GetValue(HeightRatioProperty); }
+            set { SetValue(HeightRatioProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeightRatioProperty =
+            DependencyProperty.Register(nameof(HeightRatio), typeof(double), typeof(WindowPanel), new PropertyMetadata(.5d, (s, e) => ((WindowPanel)s).OnStatePropertyChanged()));
+
+        private void OnStatePropertyChanged(bool useTransitions = true)
+        {
+            var topRow = GetTemplateChild("TopRow") as RowDefinition;
+            var bottomRow = GetTemplateChild("BottomRow") as RowDefinition;
+            var ratio = Math.Min(1, Math.Max(0, HeightRatio));
+            if (topRow is not null)
+                topRow.Height = new GridLength((1 - HeightRatio)*100, GridUnitType.Star);
+            if (bottomRow is not null)
+                bottomRow.Height = new GridLength(HeightRatio*100, GridUnitType.Star);
+        }
     }
 }
